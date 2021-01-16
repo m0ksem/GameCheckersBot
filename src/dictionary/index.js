569e08cd6a7731e0a55d694f65a562de421c415c
m0ksem <m0ksem1337@gmail.com>
2021-01-16 05:18:14 +0200

    Added dinamyc languages loading.

--- src/dictionary/index.js
+++ src/dictionary/index.js
@@ -1,12 +1,28 @@
-const ru = require('./langs/ru');
-const en = require('./langs/en');
+const fs = require('fs');
+
+function normalizePath(path) {
+  return `${__dirname}/${path}`;
+}
+
+function readLangs(folder, defaultLangCode) {
+  const filePaths = fs.readdirSync(folder);
+
+  const langsArray = filePaths.map((filePath) => {
+    const json = fs.readFileSync(`${folder}/${filePath}`);
+    return JSON.parse(json);
+  });
+
+  const defaultLang = langsArray.find((lang) => lang.langCode === defaultLangCode);
+  const langs = {};
+  langsArray.forEach((lang) => { langs[lang.langCode] = { ...defaultLang, ...lang }; });
+
+  return langs;
+}
 
 module.exports = {
   DEFAULT_LANGUAGE_CODE: 'en',
 
-  langs: {
-    en, ru,
-  },
+  langs: readLangs(normalizePath('./langs'), this.DEFAULT_LANGUAGE_CODE),
 
   text(languageCode) {
     if (!Object.keys(this.langs).includes(languageCode)) {
